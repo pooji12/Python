f9555702d8ad29c814859f1eb4a18d857b4aa80e
pooji12 <67092371+pooji12@users.noreply.github.com>
2020-06-18 21:43:10 +0530

    Add files via upload

---
+++ Python_Assignment.py
@@ -0,0 +1,91 @@
+Python_Assignment_1
+
+#R.Poojitha-321810304056
+
+
+
+# to design simple calculator
+x,y=10,10           #Addition
+print(x,y)
+
+x=15
+y=15
+z=x+y
+print(z)
+
+x,y=20,10       #subtraction
+print(x-y)
+
+x,y=5,3           #Multiplication
+print(x*y)
+
+x,y=60,30         #division
+print(x/y)
+
+x,y=100,15           #modulus
+print(x%y)
+
+x=4**4             #exponentiation
+print(x)
+
+x,y=60,30         #floor division
+print(x//y)
+
+
+
+# Simple Interest(si)
+p=150
+t=14
+r=3
+si=p*t*r/100
+print(si)
+
+
+#Area Of a Circle
+PI=3.14
+r=5
+Area=PI*r*r
+print(Area)
+
+#Area of  Triangle
+a=10
+b=4
+Area=(a*b/2)
+print(Area)
+
+
+#Temperature in celcius to fahrenheit
+celcius=22.5
+fahrenheit=(celcius*1.8)+32
+print(fahrenheit)
+
+#Area of rectangle
+w=5
+l=8
+Area=w*l
+print(Area)
+
+#Perimeter of a square
+a=20
+P=4*a
+print(P)
+
+#circumference of a circle
+PI=3.14
+r=4
+C=2*PI*r
+print(C)
+
+
+#Swapping of Two numbers
+x,y=10,30
+temp=x
+x=y
+y=temo
+print(x,y)
+  
+x=input('Enter value of x :')
+y=input('Enter value of y :')
+x,y=y,x
+print(x)
+print(y)
