8a9c811accc91d179c9add549d45149fc53be038
pooji12 <67092371+pooji12@users.noreply.github.com>
2020-06-21 21:08:35 +0530

    Add files via upload

---
+++ 321810304056_Suppose passing marks of a subject is 35.Take input of marks from user and check whether it is greater than passing marks or not.py
@@ -0,0 +1,12 @@
+
+
+
+# ## Suppose passing marks of a subject is 35.Take input of marks from user and check whether it is greater than passing marks or not
+
+
+
+marks=float(input("Enter marks of the student"))
+if marks>=35:
+    print("Student has passed in the subject")
+else:
+    print("Student has failed in the subject")
