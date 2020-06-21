6c05310036b5d4c32da5647c132417de240d0b72
pooji12 <67092371+pooji12@users.noreply.github.com>
2020-06-21 21:09:14 +0530

    Add files via upload

---
+++ 321810304056-1.Take two inputs from user and check whether they are equal or not.py
@@ -0,0 +1,13 @@
+
+
+
+###Take two inputs from user and check whether they are equal or not
+
+
+
+str1=str(input("Enter the first string:"))
+str2=str(input("Enter the second string:"))
+if str1==str2:
+    print("Yes Both strings are equal")
+else:
+    print("No Both strings are not equal")
