f90f4625c4e5cce5439e45e106bb967c697cfb34
pooji12 <67092371+pooji12@users.noreply.github.com>
2020-06-21 21:12:12 +0530

    Add files via upload

---
+++ 321810304056.py
@@ -0,0 +1,15 @@
+
+
+
+### Take two number and check whether the sum is greater than 5,less than 5 or equal to 5
+
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=a+b
+if c>5:
+    print("Sum is greater than 5")
+elif c<5:
+     print("Sum is less than 5")
+else:
+    print("Sum is equal to 5")
